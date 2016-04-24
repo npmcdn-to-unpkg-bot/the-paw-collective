1f3c100a3faf14fd135e828d7d98bf57a29e73c5
Yuujin Lee <yuujin.lee@gmail.com>
2016-04-24 15:47:48 +0200

    Separate image uploader module into a service

--- client/app/admin-animal/services.js
+++ client/app/admin-animal/services.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('animalCollectiveApp.animals')
-    .service('adminAnimalDataService', function($http, $q, Upload) {
+    .service('adminAnimalDataService', function($http, $q, Upload, imageUploader) {
         // AngularJS will instantiate a singleton by calling "new" on this function
 
         this.queue = []
@@ -14,31 +14,9 @@ angular.module('animalCollectiveApp.animals')
             $http.get(`/api/animal/${id}`).then(cb)
         }
 
-        // Upload Image Module
-        this.uploadImage = (file, cb) => {
-            // Upload Image Here
-            console.log('the image trying to upoad', file)
-
-            Upload.upload({
-                url: 'api/image-uploads',
-                data: {
-                    file: file,
-                    'username': 'user-animal'
-                }
-            }).then((resp) => {
-                return $q.resolve(resp.data.url).then(cb)
-            }, (resp) => {
-                console.log('Error status: ' + resp.status)
-            }, (evt) => {
-                let progressPercentage = parseInt(100.0 * evt.loaded / evt.total)
-                this.exporting = true
-            })
-        }
-
         this.updateAPI = (animal, file, cb) => {
 
-            // Make a post with data (Animal)
-            this.uploadImage(file, (result) => {
+            imageUploader.uploadImage(file, (result) => {
                 animal.image = result
                 let request = $http.post('/api/animal', animal)
                 this.queue.push(request)
@@ -53,7 +31,7 @@ angular.module('animalCollectiveApp.animals')
             if (imageFile !== animal.data.image) {
                 console.log('image does not match!!!')
 
-                this.uploadImage(imageFile, (result) => {
+                imageUploader.uploadImage(imageFile, (result) => {
                     animal.image = result
                     $http.put(`/api/animal/${id}`, animal)
                 })
---
+++ client/components/image-upoad/image-upload.service.js
@@ -0,0 +1,30 @@
+'use strict';
+
+(function() {
+
+    function imageUploader($http, $q, Upload) {
+
+        this.uploadImage = (file, cb) => {
+        
+            Upload.upload({
+                url: 'api/image-uploads',
+                data: {
+                    file: file,
+                    'username': 'user-animal'
+                }
+            }).then((resp) => {
+                return $q.resolve(resp.data.url).then(cb)
+            }, (resp) => {
+                console.log('Error status: ' + resp.status)
+            }, (evt) => {
+                let progressPercentage = parseInt(100.0 * evt.loaded / evt.total)
+                this.exporting = true
+            })
+        }
+
+    }
+
+    angular.module('animalCollectiveApp.animals')
+        .service('imageUploader', imageUploader)
+
+})()
